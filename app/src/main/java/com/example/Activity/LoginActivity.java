d021ee0fc9e810ba873757a97411558bdb48e5f9
DESKTOP-KS198EQ\User <13circle97@gmail.com>
2017-01-20 06:31:34 +0900

    Synopsis By Jooho Lee (Jan. 20th, 2017 6:30 AM)
    1) 사용자명과 비밀번호가 존재하고, 입력이 되었는지를 체크하는 함수를 추가
    
    - Prototype: public boolean chk_ID_PW(EditText id, EditText pw);
    - 일단은 글자의 입력 유무만 판단하도록 조건을 걸어둠
    - 후에 데이터베이스와 연동 시에 활용할 수 있도록 하는 것이 주 목적
    
    2) 비밀번호 입력용 EditText 위젯 속성 변경
    
    - Plain Text에서 Password로 변경 (보안)

--- app/src/main/java/com/example/Activity/LoginActivity.java
+++ app/src/main/java/com/example/Activity/LoginActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.Activity.R.id.sign_In_Button;
 import static com.example.Activity.R.id.sign_Up_Button;
@@ -13,18 +15,27 @@ import static com.example.Activity.R.id.sign_Up_Button;
 public class LoginActivity extends AppCompatActivity {
     public Button log_In_Button;
     public TextView join_Button;
+    EditText username;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        username = (EditText)findViewById(R.id.username_Input);
+        password = (EditText)findViewById(R.id.password_Input);
+
         log_In_Button = (Button)findViewById(sign_In_Button);
         log_In_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), InActivity.class);
-                startActivity(intent);
+
+                //Limited Access to clarify whether the information is corresponding with DB
+                if(chk_ID_PW(username, password)) {
+                    Intent intent = new Intent(getApplicationContext(), InActivity.class);
+                    startActivity(intent);
+                }
             }
         });
 
@@ -38,11 +49,31 @@ public class LoginActivity extends AppCompatActivity {
         });
 
     }
+
+    public boolean chk_ID_PW(EditText id, EditText pw) {
+        String ID = id.getText().toString();
+        String PW = pw.getText().toString();
+
+        if(ID.length() == 0 || PW.length() == 0) {
+            Toast.makeText(getApplicationContext(), "Please enter username and password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        /* TODO: Relate with DB Information (b/w Registered information and Username & Password)
+        *  As the purpose of this method is exception handling,
+        *  DO NOT FORGET TO RETURN THE BOOLEAN VALUE OF DECISION
+        *  (true or false)
+        * */
+
+
+        //If the whole conditions are satisfied
+        return true;
+    }
+}
+
 /*
     public void signIn(View v){
         Toast.makeText(getApplicationContext(), "로그인 완료", Toast.LENGTH_LONG).show();
         finish();
     }
 */
-
-}
