53242a57e52d82b288d063af5619fcae15ceb1c3
jiwoo <wldn3634@naver.com>
2017-01-19 20:06:47 +0900

    로그인 회원가입 연결, 회원가입 submit 후 '회원가입 완료' toast 추가

--- app/src/main/java/com/example/Activity/LoginActivity.java
+++ app/src/main/java/com/example/Activity/LoginActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import static com.example.Activity.R.id.sign_In_Button;
+import static com.example.Activity.R.id.sign_Up_Button;
 
 public class LoginActivity extends AppCompatActivity {
     public Button log_In_Button;
+    public TextView join_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,21 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
+
+        join_Button = (TextView)findViewById(sign_Up_Button);
+        join_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    public void signIn(View v){
+        Toast.makeText(getApplicationContext(), "로그인 완료", Toast.LENGTH_LONG).show();
+        finish();
     }
 
 
