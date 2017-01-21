60ae0cd5a3895299212eface8ceb180c380a7afe
Jooho Daniel Lee <13circle97@gmail.com>
2017-01-21 19:30:03 +0900

    Synopsis By Jooho Lee (Jan. 21th, 2017 | 7:30 PM)
    1) ID/PW 찾기 관련 액티비티 추가
    
    - 일단 취소 버튼만 추가됨
    - FindIdPwActivity.java, activity_find_id_pw.xml
    
    2) java/com.example 에 새로운 디렉터리 추가됨
    
    - 왜 추가되었는지 알 수 없음
    - 실행 및 빌드에 문제 없음

---
+++ app/src/main/java/com/example/kjw90/ucam/FindIdPwActivity.java
@@ -0,0 +1,13 @@
+package com.example.kjw90.ucam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindIdPwActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_pw);
+    }
+}
