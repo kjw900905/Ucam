595728db5704fdf0ef7fa11c289ccb0573463627
jiwoo <wldn3634@naver.com>
2017-01-30 06:01:46 +0900

    --SearchSchoolDialog, activity_search_shcool_dialog 만듬--
    1. 회원가입 부분에서 체크박스를 라디오버튼으로 바꿈
    
    2. submit 버튼 누르면 insert(db에 이름, 아이디, 비번, 학번, 학교 저장 테스트 완료)      ***성별은 실패
    
    3.  ㄱ. 학교는 타이핑불가(검색해서 값을 가져올수만 있음)
         ㄴ. 학교 검색 버튼 누르면 학교 검색하는 다이얼로그 띄움
         ㄷ. 다이얼로그 안에서 selectOne을 사용하여 학교검색
         ㄹ. selectOne으로 검색한 결과 listview에 나오면 원하는 항목을 클릭하면 값을 가져와서 학교칸에 값이 들어감.
    
    4. 메니페스트에 SearchSchoolDialog추가, 인터넷 허가설정 추가

--- app/src/main/java/com/example/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/Activity/SignUpActivity.java
@@ -1,45 +1,59 @@
 package com.example.Activity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText name, id, pw, studNum, univName;
-    CheckBox male, female;
+    EditText School_Name;
+
+    private static final int LAUNCHED_ACTIVITY = 1;
+
+    String name2;
+    private static final String SERVER_URL = "http://192.168.35.16/";
+    private static final String INSERT_PHP = "Insert_personal_information.php";
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        String name = data.getStringExtra("name");
+        School_Name.setText(name);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        male = (CheckBox)findViewById(R.id.check_Box_M);
-        male.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "남성", Toast.LENGTH_SHORT).show();
-            }
-        });
+        School_Name = (EditText) findViewById(R.id.input_School_Name);
+        School_Name.setFocusable(false);
+        School_Name.setClickable(false);
 
-        female = (CheckBox)findViewById(R.id.checkBoxW);
-        female.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "여성", Toast.LENGTH_SHORT).show();
-            }
-        });
+        // Toast.makeText(getApplicationContext(), name2, Toast.LENGTH_SHORT).show();
+
     }
 
-    public void submit_Button(View v){
-        name = (EditText)findViewById(R.id.input_Name);
-        id = (EditText)findViewById(R.id.input_Id);
-        pw = (EditText)findViewById(R.id.input_Pw);
-        studNum = (EditText)findViewById(R.id.input_Stu_Num);
-        univName = (EditText)findViewById(R.id.input_School_Name);
+    public void submit_Button(View v) {
+        name = (EditText) findViewById(R.id.input_Name);
+        id = (EditText) findViewById(R.id.input_Id);
+        pw = (EditText) findViewById(R.id.input_Pw);
+        studNum = (EditText) findViewById(R.id.input_Stu_Num);
+        univName = (EditText) findViewById(R.id.input_School_Name);
 
         String Name = name.getText().toString();
         String ID = id.getText().toString();
@@ -47,14 +61,100 @@ public class SignUpActivity extends AppCompatActivity {
         String StudNum = studNum.getText().toString();
         String UnivName = univName.getText().toString();
 
-        if(chkString(Name, ID, PW, StudNum, UnivName) && chkMaleFemale(male, female)) {
+        if (chkString(Name, ID, PW, StudNum, UnivName)) {
             Toast.makeText(getApplicationContext(), "회원가입 완료", Toast.LENGTH_LONG).show();
+
+            //이부분에 DB구문을 써주면 됨.
+            //Insert(Name, ID, PW, StudNum, UnivName); 이 형식대로 하면 됨.
+            EditText edtName = (EditText) findViewById(R.id.input_Name);
+            EditText edtId = (EditText) findViewById(R.id.input_Id);
+            EditText edtPw = (EditText) findViewById(R.id.input_Pw);
+            EditText edtStuNum = (EditText) findViewById(R.id.input_Stu_Num);
+            EditText edtSchoolName = (EditText) findViewById(R.id.input_School_Name);
+
+            String strName = edtName.getText().toString();
+            String strId = edtId.getText().toString();
+            String strPw = edtPw.getText().toString();
+            String strStuNum = edtStuNum.getText().toString();
+            String strSchoolName = edtSchoolName.getText().toString();
+
+            Insert(strName, strId, strPw, strStuNum, strSchoolName);
+
             finish();
         }
     }
+    public void Insert(String strName, String strId ,String strPw, String strStuNum, String strSchoolName) {
+        class InsertTask extends AsyncTask<String, Void, String> {
+            /*ProgressDialog loading;
 
-    public void search_Button(View v){
-        Toast.makeText(getApplicationContext(), "검색중...", Toast.LENGTH_SHORT).show();
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Please Wait", null, true, true);
+            }
+            */
+
+            protected String doInBackground(String[] params) {
+                String name = (String) params[0];
+                String id = (String) params[1];
+                String password = (String) params[2];
+                String studentNumber = (String) params[1];
+                String schoolName = (String) params[2];
+
+                try {
+                    String data = "";
+                    data += URLEncoder.encode("name", "UTF-8") + "=" + URLEncoder.encode(name, "UTF-8");
+                    data += "&" + URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
+                    data += "&" + URLEncoder.encode("password", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8");
+                    data += "&" + URLEncoder.encode("studentNumber", "UTF-8") + "=" + URLEncoder.encode(studentNumber, "UTF-8");
+                    data += "&" + URLEncoder.encode("schoolName", "UTF-8") + "=" + URLEncoder.encode(schoolName, "UTF-8");
+
+                    URL url = new URL(SERVER_URL + INSERT_PHP);
+                    URLConnection con = url.openConnection();
+
+                    con.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+
+
+                    // Read Server Response
+                    while((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+
+                    return sb.toString().trim();
+                } catch(Exception exception) {
+                    return new String(exception.getMessage());
+                }
+            }
+
+            /*
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+            */
+        }
+
+        InsertTask insertTask = new InsertTask();
+        insertTask.execute(strName, strId, strPw, strStuNum, strSchoolName);
+    }
+
+    public void search_Button(View v) {
+
+        Intent intent = new Intent(getApplicationContext(), SearchSchoolDialog.class);
+        startActivityForResult(intent, LAUNCHED_ACTIVITY);
+
+        //Toast.makeText(getApplicationContext(), "검색중...", Toast.LENGTH_SHORT).show();
 
         /* TODO: 검색 화면으로 넘어가기
         *  1) DB와 연동하여 검색
@@ -62,11 +162,11 @@ public class SignUpActivity extends AppCompatActivity {
         * */
     }
 
-    private boolean chkString(String Name, String ID, String PW, String StudNum, String UnivName){
-        int[] chk = { Name.length(), ID.length(), PW.length(), StudNum.length(), UnivName.length() };
+    private boolean chkString(String Name, String ID, String PW, String StudNum, String UnivName) {
+        int[] chk = {Name.length(), ID.length(), PW.length(), StudNum.length(), UnivName.length()};
 
-        for(int i = 0; i < 5; i++){
-            if(chk[i] <= 0){
+        for (int i = 0; i < 5; i++) {
+            if (chk[i] <= 0) {
                 Toast.makeText(getApplicationContext(), "정보를 모두 입력해 주세요.", Toast.LENGTH_SHORT).show();
                 return false;
             }
@@ -80,25 +180,7 @@ public class SignUpActivity extends AppCompatActivity {
         return true;
     }
 
-    private boolean chkMaleFemale(CheckBox male, CheckBox female){
-        boolean isMaleChecked = male.isChecked();
-        boolean isFemaleChecked = female.isChecked();
-
-        if(isMaleChecked && isFemaleChecked){
-            Toast.makeText(getApplicationContext(),
-                    "양성애자가 아니시라면 한 가지 성별만 선택하십시오.\n" +
-                            "양성애자는 가입이 승인되지 않습니다.", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        else if(isMaleChecked || isFemaleChecked){
-            return true;
-        }
-
-        Toast.makeText(getApplicationContext(), "성별을 체크해 주세요.", Toast.LENGTH_SHORT).show();
-        return false;
-    }
-
-    public void cancel_Button(View v){
+    public void cancel_Button(View v) {
         Toast.makeText(getApplicationContext(), "가입 취소", Toast.LENGTH_SHORT).show();
         finish();
     }
