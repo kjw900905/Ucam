651a50da5320424b5fffe7c70509e4ce9a5963a4
Jaewoo Kim <rhdqud7296@naver.com>
2017-02-01 13:48:46 +0900

    타임테이블 추가

---
+++ app/src/main/java/com/example/Activity/DrawTimeTable.java
@@ -0,0 +1,17 @@
+package com.example.Activity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class DrawTimeTable extends View {
+    public DrawTimeTable(Context context) {
+        super(context);
+    }
+
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+    }
+}
