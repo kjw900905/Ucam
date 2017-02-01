f6acf32c3a6c7b6440eb6728ce75cfe03fce7755
Jooho Lee <13circle97@gmail.com>
2017-02-01 21:41:01 +0900

    Variable 및 회원정보 수정 관련 클래스 수정

--- app/src/main/java/com/example/Activity/EditMemInfoFragment.java
+++ app/src/main/java/com/example/Activity/EditMemInfoFragment.java
@@ -8,11 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
+import com.example.Beans.Student;
 import com.example.Beans.Variable;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -20,11 +26,16 @@ import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class EditMemInfoFragment extends Fragment {
 
     EditText name, id, pw, studNum, univName;
     RadioGroup rdGroupEdit;
+    JSONArray peoples = null;
+    Student studData;
+    String myJSON;
 
     public EditMemInfoFragment() {
         // Required empty public constructor
@@ -36,15 +47,23 @@ public class EditMemInfoFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_edit_mem_info, container, false);
 
-        name = (EditText)view.findViewById(R.id.input_Name_Edit);
-        id = (EditText)view.findViewById(R.id.input_Id_Edit);
-        pw = (EditText)view.findViewById(R.id.input_Pw_Edit);
-        studNum = (EditText)view.findViewById(R.id.input_Stu_Num_Edit);
-        univName = (EditText)view.findViewById(R.id.input_School_Name_Edit);
-        rdGroupEdit = (RadioGroup)view.findViewById(R.id.rdGroup_Edit);
+        /* TODO: ID를 InActivity로부터 받아서 탐색.
+        *  - 현 상태는 null로 초기화
+        *  - SelectOne 내부의 showStudInfo 함수 자료탐색 오류 수정 필요
+        * */
+        String Searching_ID = null;
+
+        name = (EditText) view.findViewById(R.id.input_Name_Edit);
+        id = (EditText) view.findViewById(R.id.input_Id_Edit);
+        pw = (EditText) view.findViewById(R.id.input_Pw_Edit);
+        studNum = (EditText) view.findViewById(R.id.input_Stu_Num_Edit);
+        univName = (EditText) view.findViewById(R.id.input_School_Name_Edit);
+        rdGroupEdit = (RadioGroup) view.findViewById(R.id.rdGroup_Edit);
+
+        SelectOne(Searching_ID);
 
         // Inflate the layout for this fragment
-        Button btnSearch = (Button)view.findViewById(R.id.search_School_Edit);
+        Button btnSearch = (Button) view.findViewById(R.id.search_School_Edit);
         btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,17 +72,19 @@ public class EditMemInfoFragment extends Fragment {
             }
         });
 
-        Button btnUnregister = (Button)view.findViewById(R.id.unregister_Button);
+        Button btnUnregister = (Button) view.findViewById(R.id.unregister_Button);
         btnUnregister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getActivity(), "회원탈퇴 완료\n이전 정보는 열람할 수 없습니다.", Toast.LENGTH_SHORT).show();
-                //TODO: 회원탈퇴 관련 코드 추가
+
+                Delete(id.getText().toString());
+
                 getActivity().onBackPressed();
             }
         });
 
-        Button btnSubmit = (Button)view.findViewById(R.id.submit_Edit);
+        Button btnSubmit = (Button) view.findViewById(R.id.submit_Edit);
         btnSubmit.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -74,17 +95,17 @@ public class EditMemInfoFragment extends Fragment {
                 String StudNum = studNum.getText().toString();
                 String UnivName = univName.getText().toString();
                 int radioCheck = rdGroupEdit.getCheckedRadioButtonId();
-                RadioButton rb = (RadioButton)container.findViewById(radioCheck);
+                RadioButton rb = (RadioButton) container.findViewById(radioCheck);
                 String Gender = rb.getText().toString();
 
-                if(chkString_Edit(Name, ID, PW, StudNum, UnivName, Gender)) {
+                if (chkString_Edit(Name, ID, PW, StudNum, UnivName, Gender)) {
                     Toast.makeText(getActivity(), "회원정보 수정 완료", Toast.LENGTH_LONG).show();
                     getActivity().onBackPressed();
                 }
             }
         });
 
-        Button btnCancel = (Button)view.findViewById(R.id.cancel_Edit);
+        Button btnCancel = (Button) view.findViewById(R.id.cancel_Edit);
         btnCancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -96,15 +117,19 @@ public class EditMemInfoFragment extends Fragment {
         return view;
     }
 
-    private boolean chkString_Edit(String Name, String ID, String PW, String StudNum, String UnivName, String Gender){
-        String[] chk = { Name, ID, PW, StudNum, UnivName, Gender };
+    private boolean chkString_Edit(String Name, String ID, String PW, String StudNum, String UnivName, String Gender) {
+        String[] chk = {Name, ID, PW, StudNum, UnivName, Gender};
 
-        for(int i = 0; i < 6; i++){
-            if(chk[i].length() <= 0){
+        for (int i = 0; i < 6; i++) {
+            if (chk[i].length() <= 0) {
 
                 Toast.makeText(getActivity(), "정보를 모두 입력해 주세요.", Toast.LENGTH_SHORT).show();
                 return false;
             }
+        }
+
+        if(Gender != "Male" || Gender != "Female") {
+            return false;
         }
 
         Update(Name, ID, PW, StudNum, UnivName, Gender);
@@ -135,7 +160,7 @@ public class EditMemInfoFragment extends Fragment {
                 String password = (String) params[2];
                 String studentNumber = (String) params[3];
                 String schoolName = (String) params[4];
-                String gender = (String)params[5];
+                String gender = (String) params[5];
 
                 try {
                     // 서버에 넘겨주기 위한 데이터를 서버에서 읽을 수 있는 내용으로 인코딩하는 작업
@@ -166,14 +191,14 @@ public class EditMemInfoFragment extends Fragment {
                     String line = null;
 
                     // Read Server Response
-                    while((line = reader.readLine()) != null) {
+                    while ((line = reader.readLine()) != null) {
                         sb.append(line);
                         break;
-                }
+                    }
 
                     // onPostExecute 부분으로 스트링을 전달함
                     return sb.toString().trim();
-                } catch(Exception exception) {
+                } catch (Exception exception) {
                     return new String(exception.getMessage());
                 }
             }
@@ -190,20 +215,142 @@ public class EditMemInfoFragment extends Fragment {
         updateTask.execute(strName, strId, strPw, strStuNum, strSchoolName, gender);
     }
 
-    private void showText(EditText name, EditText id, EditText pw, EditText studNum, EditText univName, RadioGroup rgEdit){
-        //TODO: 회원의 정보를 먼저 출력하도록 DB와 연동
+    public void SelectOne(String strId) {
+        class SelectOneTask extends AsyncTask<String, Void, String> {
+            /*ProgressDialog loading;
 
-        //일단은 임시 정보로 초기화.
-        String Name = "Test Name";
-        String ID = "Test ID";
-        String PW = "Test PW";
-        String StudNum = "11111111";
-        String UnivName = "띵지머";
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Please Wait", null, true, true);
+            }
+            */
 
-        name.setText(Name);
-        id.setText(ID);
-        pw.setText(PW);
-        studNum.setText(StudNum);
-        univName.setText(UnivName);
+            protected String doInBackground(String[] params) {
+                String id = (String) params[0];
+
+                try {
+                    String data = "";
+                    data += URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
+
+                    URL url = new URL(Variable.m_SERVER_URL + Variable.m_PHP_SELECT_ONE);
+                    URLConnection con = url.openConnection();
+
+                    con.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+
+                    return sb.toString().trim();
+                } catch(Exception exception) {
+                    return new String(exception.getMessage());
+                }
+            }
+
+            protected  void onPostExecute(String result) {
+                myJSON = result;
+                showStudInfo();
+            }
+        }
+
+        SelectOneTask selectOneTask = new SelectOneTask();
+        selectOneTask.execute(strId);
+    }
+
+    private void showStudInfo() {
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray("result");
+
+            JSONObject c = peoples.getJSONObject(0);
+            String NAME = c.getString(Variable.m_TAG_NAME);
+            String ID = c.getString(Variable.m_TAG_ID);
+            String PW = c.getString(Variable.m_TAG_PW);
+            String STUD_NUM = c.getString(Variable.m_TAG_STUD_NUM);
+            String SCHOOL = c.getString(Variable.m_TAG_SCHOOL);
+            String GENDER = c.getString(Variable.m_TAG_GENDER);
+
+            name.setText(NAME);
+            id.setText(ID);
+            pw.setText(PW);
+            studNum.setText(STUD_NUM);
+            univName.setText(SCHOOL);
+
+            if(GENDER == "Male") {
+                rdGroupEdit.check(R.id.rd_Male_Edit);
+            } else if(GENDER == "Female") {
+                rdGroupEdit.check(R.id.rd_Female_Edit);
+            }
+
+        } catch(Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public void Delete(String strId) {
+        class DeleteTask extends AsyncTask<String, Void, String> {
+            /*ProgressDialog loading;
+
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(MainActivity.this, "Please Wait", null, true, true);
+            }
+            */
+
+            protected String doInBackground(String[] params) {
+                String id = (String) params[0];
+
+                try {
+                    String data = "";
+                    data += URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8");
+
+                    URL url = new URL(Variable.m_SERVER_URL + Variable.m_PHP_DELETE);
+                    URLConnection con = url.openConnection();
+
+                    con.setDoOutput(true);
+                    OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
+
+                    wr.write(data);
+                    wr.flush();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    StringBuilder sb = new StringBuilder();
+                    String line = null;
+
+                    // Read Server Response
+                    while((line = reader.readLine()) != null) {
+                        sb.append(line);
+                        break;
+                    }
+
+                    return sb.toString().trim();
+                } catch(Exception exception) {
+                    return new String(exception.getMessage());
+                }
+            }
+
+            /*
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                loading.dismiss();
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+            }
+            */
+        }
+
+        DeleteTask deleteTask = new DeleteTask();
+        deleteTask.execute(strId);
     }
 }
